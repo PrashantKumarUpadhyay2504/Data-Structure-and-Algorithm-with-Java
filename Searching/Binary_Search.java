da9f4a0f267721e378cb0d771ce82af3d3da656a
Prashant Kumar Upadhyay <78552170+2507Prashantkumarupadhyay@users.noreply.github.com>
2022-06-20 23:01:37 +0530

    Add files via upload
    Binary Search: This algorithm search element in a sorted array by repeatedly dividing the search interval in half. 
    Begin with an interval covering the whole array.
    
    1.Compare x with the middle element.
    2.If x matches with middle element, we return the mid index.
    3.Else If x is greater than the mid element, then x can only lie in the right half subarray after the mid element. 
    So we recur for right half.
    4.Else (x is smaller) recur for the left half.

---
+++ Searching/Binary_Search.java
@@ -0,0 +1,34 @@
+
+class Binary_Search {
+	
+	static int binary_Search(int arr[], int x)
+	{
+		int l = 0, r = arr.length - 1;
+		while (l <= r) {
+			int m = l + (r - l) / 2;
+
+			if (arr[m] == x)
+				return m;
+
+			if (arr[m] < x)
+				l = m + 1;
+
+			else
+				r = m - 1;
+		}
+
+		return -1;
+	}
+
+	public static void main(String args[])
+	{
+		int arr[] = { 2, 3, 4, 10, 40 };
+		int n = arr.length;
+		int x = 10;
+		int result = binary_Search(arr, x);
+		if (result == -1)
+			System.out.println("Element not present");
+		else
+			System.out.println("Element found at "+ "index " + result);
+	}
+}
